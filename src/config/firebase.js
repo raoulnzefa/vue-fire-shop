0ff09cb853df9a3fe9370a0937638ce1792ed92d
marluanespiritusantoguerrero <marluanespiritusanto>
2019-05-12 22:07:20 -0400

    initial commit

---
+++ src/config/firebase.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyBMtg8rCgq2S7JSjdk1Kd0UY3U3EYOoQa8",
+  authDomain: "vue-firestore-70ffe.firebaseapp.com",
+  databaseURL: "https://vue-firestore-70ffe.firebaseio.com",
+  projectId: "vue-firestore-70ffe",
+  storageBucket: "vue-firestore-70ffe.appspot.com",
+  messagingSenderId: "837010299523",
+  appId: "1:837010299523:web:9188893146c177ad"
+};
--- src/main.js
+++ src/main.js
@@ -1,12 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+// firebase setup
+import firebase from "firebase";
+import "firebase/firestore";
+import fireConfig from "./config/firebase";
+firebase.initializeApp(fireConfig);
+export const db = firebase.firestore();
+
+// i18n setup
+import i18n from "./config/i18n";
+
+// vuetify setup
+require("./config/vuetify");
+
+Vue.config.productionTip = false;
 
 new Vue({
+  i18n,
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
